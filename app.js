6d136dfe0a940cfd3f7db02c18357c83f9e3e1b8
Wanglala <sue4100035045@gmail.com>
2020-11-19 00:27:10 +0800

    modify port in app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const db = require("./models");
-const app = express();
-const port = 3000;
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const cors = require("cors");
+const express = require('express')
+const db = require('./models')
+const app = express()
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const cors = require('cors')
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
-const passport = require("./config/passport.js");
+const passport = require('./config/passport.js')
 
-app.use(cors());
+app.use(cors())
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(methodOverride('_method'))
 
-require("./routes/index.js")(app);
+require('./routes/index.js')(app)
 
 app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+  console.log(`http://localhost:${port}`)
+})
